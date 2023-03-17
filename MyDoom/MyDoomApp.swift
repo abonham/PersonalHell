3e5840d8f94dff0255a3989be51e0208169e135c
Aaron Bonham <aaron.bonham@sbs.com.au>
2023-03-17 14:13:54 +1100

    Initial Commit

---
+++ MyDoom/MyDoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDoomApp.swift
+//  MyDoom
+//
+//  Created by Aaron Bonham on 17/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyDoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
